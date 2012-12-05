41b66074839f9b579d62ef5c0ad66f6a4ff5b8e8
Daniel Lobato <elobatocs@gmail.com>
2012-12-05 16:44:55 +0100

    Solved git conflicts

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 gem 'rspec-puppet'
+gem 'puppetlabs_spec_helper'
 gem 'landb'
+gem 'puppet', ">= 2.7"
--- manifests/client.pp
+++ manifests/client.pp
@@ -10,24 +10,6 @@ class landb::client {
     }   
 
     file{'/etc/puppet/landb_config.yaml':
-<<<<<<< HEAD
-      ensure  => file,
-      content => template('landb/landb_config.yaml'),
-      owner   => root,
-      group   => root,
-      mode    => 0644,
-      require  => File['/etc/puppet'],
-    }  
-
-    $hash = landb_func({
-        "method"           => "get_my_device_info",
-        "method_arguments" => "",
-        "response_info"    => [["device_info", "location", "building"]],
-                              [["device_info", "location", "floor"]], 
-                              [["device_info", "location", "room"]]})
-
-    notify { $hash: } 
-=======
       ensure  => file, 
       content => template('landb/landb_config.yaml.erb'),
       owner   => root,
@@ -35,5 +17,4 @@ class landb::client {
       mode    => 0644,
       replace => true
     }  
->>>>>>> bafed135ac30a2a40358db1dc0efeb76b663a551
 } 
