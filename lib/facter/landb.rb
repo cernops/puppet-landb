99ea251688fda1f43c171152b78c5cf63bf35cda
Daniel Lobato <elobatocs@gmail.com>
2013-01-29 16:49:00 +0100

    Added caching, rack location, and network_domain facts

--- lib/facter/landb.rb
+++ lib/facter/landb.rb
@@ -1,12 +1,13 @@
-require 'yaml'
 require 'landb'
+require 'time'
 
 class LandbWrapper
-  attr_reader :client
+  attr_reader :client, :device_info
 
   def initialize
     set_landb_config
-    @client = landb_client
+    @client      = landb_client
+    @device_info = client.get_my_device_info([]).device_info
   end
 
   def set_landb_config
@@ -23,36 +24,126 @@ class LandbWrapper
   end
 
   def add_landb_location
-    location = @client.get_my_device_info([]).device_info.location
+    location = @device_info.location
 
     Facter.add("landb_location") do 
       setcode do 
-        "#{location.building}-#{location.floor}-#{location.room}"
+        "#{location.building} #{location.floor}-#{location.room}"
       end
     end         
+
+    "#{location.building} #{location.floor}-#{location.room}"
   end
 
-  def add_responsible_info
-    responsible = @client.get_my_device_info([]).device_info.responsible_person
+  def add_rackname
+    zone = @device_info.zone 
+
+    Facter.add("landb_rackname") do
+      setcode do
+        "#{zone}"
+      end
+    end
+
+    "#{zone}"
+  end
+
+  def add_responsible_email
+    responsible = @device_info.responsible_person
+
+    Facter.add("landb_responsible_email") do 
+      setcode do 
+        "#{responsible.email}"
+      end
+    end  
+
+    "#{responsible.email}"
+  end
+
+  def add_responsible_name
+    responsible = @device_info.responsible_person
 
     Facter.add("landb_responsible_name") do 
       setcode do 
         "#{responsible.first_name} #{responsible.name}"
       end
     end          
+     
+    "#{responsible.first_name} #{responsible.name}"
+  end
 
-    Facter.add("landb_responsible_email") do 
-      setcode do 
-        "#{responsible.email}"
+  def add_network_domain
+    interfaces = @device_info.interfaces
+    # GPN is assumed if there is no interface information in landb
+    domain = "GPN"
+    if !interfaces.nil?
+      if interfaces.interface_information.kind_of?(Array)
+        # If there are multiple interfaces, the first one is chosen
+        domain = interfaces.interface_information.first[:network_domain_name]
+      else
+        domain = interfaces.interface_information.network_domain_name
       end
-    end   
+    end 
+
+    Facter.add("landb_network_domain") do
+      setcode do
+        "#{domain}"
+      end
+    end
+
+    "#{domain}"
+  end
+
+end
+ 
+def get_landb_information
+  if File.exists?("/etc/puppet/landb_config.yaml")
+    facter_landb = LandbWrapper.new
+    landb_info   = {}
+    if !facter_landb.nil? # Could be nil if yaml file is not possible to parse
+      landb_info[:landb_location]          = facter_landb.add_landb_location
+      landb_info[:landb_responsible_name]  = facter_landb.add_responsible_name
+      landb_info[:landb_responsible_email] = facter_landb.add_responsible_email
+      landb_info[:landb_rackname]          = facter_landb.add_rackname
+      landb_info[:landb_network_domain]    = facter_landb.add_network_domain
+    end
+  end  
+
+  landb_info 
+end
+
+# -------------- Start
+
+facts_cache_dir  = '/etc/puppet/facts.d'
+landb_cache_file = facts_cache_dir + '/landb_cache.yaml'
+
+facts_cache_ttl = 432000 #1 week (in seconds)
+debug = false
+
+if File::exist?(landb_cache_file) then
+  landb_cache = YAML.load_file(landb_cache_file)
+  cache_time  = File.mtime(landb_cache_file)
+else   
+  puts "Err: failed to load landb.yaml local cache" if debug
+  landb_cache = nil
+  cache_time  = Time.at(0)
+end
+
+# Writes if cache has been purged, or fact exceeds TTL
+if !landb_cache || (Time.now - cache_time) > facts_cache_ttl
+  landb_data = get_landb_information
+  begin
+    Dir.mkdir(facts_cache_dir) if !File::exists?(facts_cache_dir)
+    File.open(landb_cache_file, 'w') do |out|
+      YAML.dump(landb_data, out)
+    end
+  rescue
+    puts "Err: failed to write landb.yaml cache file." if debug
+  end
+else
+  landb_cache = YAML.load_file(landb_cache_file)
+  landb_cache.each do |key, value|
+    Facter.add("#{key}") { setcode { "#{value}" } }
   end
 end
 
-if File.exists?("/etc/puppet/landb_config.yaml")
-  facter_landb = LandbWrapper.new
-  if !facter_landb.nil? # could be nil if yaml is not possible to parse
-    facter_landb.add_landb_location
-    facter_landb.add_responsible_info
-  end
-end
+
